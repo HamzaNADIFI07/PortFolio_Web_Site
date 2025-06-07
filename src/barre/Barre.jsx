bca20439cc599cee28ba9f6a02692913af495aeb
Hamza NADIFI <nadifihamza07@gmail.com>
2025-06-07 19:18:34 +0200

    Changement de la logique de style de la barre

--- src/barre/Barre.jsx
+++ src/barre/Barre.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
 import Style from './BarreStyles.module.css'
 import logo from '../assets/logo/logo.png'
 
 function Barre() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
       const top = section.getBoundingClientRect().top + window.scrollY - 250;
       window.scrollTo({ top, behavior: 'smooth' });
-      setIsMenuOpen(false);
     }
   };
 
@@ -19,16 +15,11 @@ function Barre() {
         <div className={Style.logo}>
             <img src={logo} alt="logo" />
         </div>
-        <div className={`${Style.button} ${isMenuOpen ? Style.active : ''}`}>
+        <div className={Style.button}>
             <button type="button" onClick={() => scrollToSection('nom')}>About</button>
             <button type="button" onClick={() => scrollToSection('skills')}>Skills</button>
             <button type="button" onClick={() => scrollToSection('projects')}>Projects</button>
             <button type="button" onClick={() => scrollToSection('contact')}>Contact</button>
-        </div>
-        <div className={Style.menu} onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            <span></span>
-            <span></span>
-            <span></span>
         </div>
     </div>
   )
